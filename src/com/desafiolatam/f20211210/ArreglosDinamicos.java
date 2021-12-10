5b4da994b10781f84d355bb4ffabb97f49141cb0
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-10 19:13:36 -0300

    20211210

---
+++ src/com/desafiolatam/f20211210/ArreglosDinamicos.java
@@ -0,0 +1,86 @@
+package com.desafiolatam.f20211210;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArreglosDinamicos {
+
+	public static void main(String[] args) {
+		ArrayList<String> alumnos = new ArrayList<String>();
+		/// List<String> alumnos = new ArrayList<String>();
+
+		// tamaño size()
+		System.out.println(alumnos.size());// the number of elements in this list
+
+		// agregar al final de la lista
+		alumnos.add("German");
+		alumnos.add("Isra");
+		alumnos.add("Hector");
+		alumnos.add("Isabel");
+		// alumnos.add("German");
+
+		System.out.println(alumnos.size());// 4
+		System.out.println(alumnos);
+
+		alumnos.add(2, "Edinson");// desplazar a derecha el resto de elementos
+		System.out.println(alumnos.size());// 4
+		System.out.println(alumnos);
+
+		// set; reemplaza el valor existente
+		String nombre = alumnos.set(1, "Joaquin");
+		// alumnos.add(0,nombre);
+		// System.out.println(nombre);
+		System.out.println(alumnos.size());//
+		System.out.println(alumnos);
+
+		/**
+		 * REMOVER
+		 */
+
+		// remover por indice
+		alumnos.remove(1);
+		System.out.println(alumnos.size());//
+		System.out.println("remove " + alumnos);
+
+		// remover por coicidencia
+		alumnos.remove("German");
+		System.out.println(alumnos.size());//
+		System.out.println("remove " + alumnos);
+		alumnos.add("zisrael");
+		alumnos.add("zisrael");
+		System.out.println(alumnos);
+		System.out.println("minimo alumnos "+ Collections.min(alumnos));
+		System.out.println("maximo alumnos "+ Collections.max(alumnos));
+		
+		/*FRECUENCY*/
+		
+		System.out.println("Lista frecuency"+alumnos);
+		System.out.println("frecuencia "+ Collections.frequency(alumnos,"zisrael"));
+		
+		alumnos.clear();// elimina todos los elementos de esta lista
+		System.out.println(alumnos.size());//
+		System.out.println(alumnos);
+
+		/* Maximo y Minimo*/
+
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		numeros.add(5);
+		numeros.add(1);
+		numeros.add(6);
+		numeros.add(4);
+		numeros.add(1);
+		numeros.add(2);
+		numeros.add(6);
+		System.out.println(numeros);
+		System.out.println("minimo "+ Collections.min(numeros));
+		System.out.println("maximo "+Collections.max(numeros));
+		
+		Collections.sort(numeros);
+		Collections.reverse(numeros);
+		System.out.println(numeros);
+		
+
+	}
+
+}
---
+++ src/com/desafiolatam/f20211210/ListaAlimentos.java
@@ -0,0 +1,49 @@
+package com.desafiolatam.f20211210;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListaAlimentos {
+
+	public static void main(String[] args) {
+		ordenar();
+	}
+	
+	public static void ordenar() {
+		List<String> comidas = new ArrayList<String>();
+		comidas.add("Cazuela");
+		comidas.add("Porotos");
+		comidas.add("Pastel de Choclo");
+		comidas.add("Ají de gallina");
+		comidas.add("Ceviche");
+		comidas.add("Arepas");
+		//orden de arreglo
+		Collections.sort(comidas);
+		
+		System.out.println(comidas);
+		
+		System.out.println("\n ** Menu del dia**\n");
+		
+		for (int i = 0; i < comidas.size(); i++) {
+			//acceder por indice a cada elemento
+			System.out.println("* "+comidas.get(i));
+		}
+
+	}
+	
+	/*
+	 * Crear un método llamado “ordenar” que nos permita 
+ordenar alfabéticamente una lista de platos de un 
+restaurante, también se debe mostrar lista ordenada por 
+pantalla.
+Esta lista cuenta con los siguientes datos: 
+● Cazuela
+● Porotos
+● Pastel de Choclo
+● Ají de gallina
+● Ceviche
+● Arepas.
+
+	 * */
+}
