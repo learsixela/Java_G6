18cb8543ab736ff47a49907aeacf42c39db942fd
alexis israel quezada palma <ipalm001@hotmail.com>
2021-12-10 18:30:45 -0300

    ejemplo dibujo

---
+++ src/com/desafiolatam/f20211210/Ejemplo.java
@@ -0,0 +1,25 @@
+package com.desafiolatam.f20211210;
+
+public class Ejemplo {
+	
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				
+				if(i==0 || i ==(n-1)) {
+					System.out.printf("*");
+				}else {
+					if(j == 0 || j ==(n-1) ) {
+						System.out.printf("*");
+					}else {
+						System.out.printf(" ");
+					}
+				}
+				
+			}
+			System.out.printf("\n");
+		}
+	}
+
+}
