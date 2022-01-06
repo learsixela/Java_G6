93de0296c4629b9a98648736f1f7f69b564661ec
alexis israel quezada palma <ipalm001@hotmail.com>
2022-01-06 18:22:43 -0300

    20220106 file reader

--- src/com/desafiolatam/f20220106/EscrituraArchivo.java
+++ src/com/desafiolatam/f20220106/EscrituraArchivo.java
@@ -1,10 +1,32 @@
 package com.desafiolatam.f20220106;
 
+import java.io.File;
+import java.io.IOException;
+
 public class EscrituraArchivo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		crearArchivo();
 
 	}
 
+	public static void crearArchivo() {
+		File archivo = new File("src/com/desafiolatam/f20220106/archivo.txt");
+		
+		try {
+			if(!archivo.exists()) {//SI No existe el archivo
+				archivo.createNewFile();//creo el archivo
+				System.out.println("Archivo creado...");
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Error en la creacion del archivo");
+		}
+		
+		
+	}
+	
+	public static void escribirArchivo() {
+		
+	}
 }
