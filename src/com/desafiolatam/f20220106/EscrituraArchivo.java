a5f1a69d03f7f79a99707371672af83f7ed8b605
alexis israel quezada palma <ipalm001@hotmail.com>
2022-01-06 21:05:04 -0300

    20220106 buscar Texto

--- src/com/desafiolatam/f20220106/EscrituraArchivo.java
+++ src/com/desafiolatam/f20220106/EscrituraArchivo.java
@@ -7,6 +7,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class EscrituraArchivo {
 
@@ -33,11 +36,40 @@ public class EscrituraArchivo {
 
 	public static void escribirArchivo() {
 		File archivo = new File("src/com/desafiolatam/f20220106/archivo.txt");
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add("Perro");
+		lista.add("Gato");
+		lista.add("Juan");
+		lista.add("Daniel");
+		lista.add("Juan");
+		lista.add("Gato");
+		lista.add("Perro");
+		lista.add("Camila");
+		lista.add("Daniel");
+		lista.add("Camila");
 
 		try {
 			FileWriter archivoWriter = new FileWriter(archivo);
 			BufferedWriter archivoBuff = new BufferedWriter(archivoWriter);
-						
+					
+			/*for (Iterator iterator = lista.iterator(); iterator.hasNext();) {
+				String texto = (String) iterator.next();
+				archivoBuff.write(texto);
+				archivoBuff.newLine();
+			}
+			for (String texto : lista) {
+				archivoBuff.write(texto);
+				archivoBuff.newLine();
+			}
+			for (int i = 0; i < lista.size(); i++) {
+				archivoBuff.write(lista.get(i));
+				if(i!=(lista.size()-1)) {
+					archivoBuff.newLine();
+				}
+			}*/
+			
+			
+			
 			archivoBuff.write("hola clase ");
 			archivoBuff.newLine();//salto de linea
 			archivoBuff.write("aprendiendo Java ");
@@ -52,11 +84,13 @@ public class EscrituraArchivo {
 			System.out.println("error al escribir");
 		}
 
+		
+		
 	}
 
 	public static void leerArchivo() {
 		File archivo = new File("src/com/desafiolatam/f20220106/archivo.txt");
-
+		int contador = 0;
 		try {
 			FileReader archivoReader = new FileReader(archivo);
 			BufferedReader archivoBuff = new BufferedReader(archivoReader);
@@ -65,9 +99,14 @@ public class EscrituraArchivo {
 			while (data != null) {
 				
 				System.out.println(data);
-				data = archivoBuff.readLine();
+				/*if(data.equals("Perro")) {
+					System.out.println("Existe perro");
+					contador++;
+				}*/
+				//System.out.println("..."+data.isBlank());				
+				data = archivoBuff.readLine();				
 			}
-			
+			//System.out.println("veces que existe en el archivo "+contador);
 			archivoBuff.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -78,6 +117,9 @@ public class EscrituraArchivo {
 		}
 		
 		
+	}
+
+	public static void buscarTexto(String nombreFichero, String texto) {
 		
 	}
 }
